331d800fa3fa46dd53dbb7cf1aa9bc0f0e8bf550
Luis Daniel <Luis Daniel@LAPTOP-LOUIS>
2023-06-24 12:44:57 -0600

    listo

---
+++ src/main/java/com/Practica3/Controller/ArbolController.java
@@ -0,0 +1,31 @@
+
+package com.Practica3.Controller;
+
+import com.Practica3.Service.ArbolService;
+import com.Practica3.domain.Arbol;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/arbol")
+@Slf4j
+public class ArbolController {
+    
+    @Autowired
+    private ArbolService arbolService;
+    
+    @RequestMapping("/listado")
+    public String page(Model model) {
+        log.info("Consumo del Recurso /arbol/listado");
+        List<Arbol> arboles = arbolService.getArboles(false);
+        model.addAttribute("Lista_Categorias", arboles);
+        model.addAttribute("Total_Categorias", arboles.size());
+        return "/arbol/listado";
+    }
+    
+    
+}
