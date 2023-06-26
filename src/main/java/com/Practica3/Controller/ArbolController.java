605cdd207940590d7c81ce5cad9a305d03a99ea0
micro <micro@Rivera>
2023-06-25 21:57:10 -0600

    ArbolController Ready

--- src/main/java/com/Practica3/Controller/ArbolController.java
+++ src/main/java/com/Practica3/Controller/ArbolController.java
@@ -1,4 +1,3 @@
-
 package com.Practica3.Controller;
 
 import com.Practica3.Service.ArbolService;
@@ -8,24 +7,64 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/arbol")
 @Slf4j
 public class ArbolController {
-    
+
     @Autowired
     private ArbolService arbolService;
-    
+
     @RequestMapping("/listado")
     public String page(Model model) {
         log.info("Consumo del Recurso /arbol/listado");
         List<Arbol> arboles = arbolService.getArboles(false);
-        model.addAttribute("Lista_Categorias", arboles);
-        model.addAttribute("Total_Categorias", arboles.size());
+        model.addAttribute("Lista_Arboles", arboles);
+        model.addAttribute("Total_Arboles", arboles.size());
         return "/arbol/listado";
     }
+
+    @GetMapping("/nuevo")
+    public String ArbolNuevo(Arbol arbol) {
+        return "/arbol/editar";
+    }
+
     
+    @Autowired
+    private FirebaseStorageServiceimpl firebaseStorageService;
     
+    @Autowired
+    @PostMapping("/guardar")
+    public String arbolGuardar(Arbol arbol, @RequestParam("imagenFile") MultipartFile imagenFile) {
+        if (!imagenFile.isEmpty()) {
+            arbolService.save(arbol);
+            arbol.setRutaImagen(
+                    firebaseStorageService.cargaImagen(
+                            imagenFile,
+                            "arbol",
+                            arbol.getIdArbol()));
+        }
+        arbolService.save(arbol);
+        return "redirect:/arbol/listado";
+    }
+
+    @GetMapping("/eliminar/{idArbol}")
+    public String ArbolEliminar(Arbol arbol) {
+        arbolService.delete(arbol);
+        return "redirect:/arbol/listado";
+    }
+
+    @GetMapping("/editar/{idArbol}")
+    public String arboleditar(Arbol arbol, Model model) {
+        arbol = arbolService.getArbol(arbol);
+        model.addAttribute("arbol", arbol);
+        return "/arbol/editar";
+    }
+
 }
--- src/main/java/com/Practica3/Service/ArbolService.java
+++ src/main/java/com/Practica3/Service/ArbolService.java
@@ -7,5 +7,12 @@ public interface ArbolService {
     
       public List<Arbol> getArboles(boolean estadoVida);
      
+      
+    public Arbol getArbol(Arbol arbol);
+    
+    public void save(Arbol arbol);
+    
+    //Se elimina un registro:
+    public void delete(Arbol arbol);
 
 }
